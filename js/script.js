d559ebd34471786db08d568fe2de81898a71db51
Fahimadnan33 <142169505+Fahimadnan33@users.noreply.github.com>
2023-11-17 22:12:46 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function textChange(){
+  document.getElementById('tc').innerHTML="fahim adnan";
+}
+//
+function dateTime(){
+  document.getElementById('date').innerHTML=Date( );
+}
+// 
+function colorChange(){
+  document.getElementById('color').style.color="red";
+}
+//s/h//
+function textHide(){
+  document.getElementById('sh').style.display="none";
+}
+function textShow(){
+  document.getElementById('sh').style.display="block";
+
+}
+//
+function nemeClass(){
+  document.getElementsByClassName('h2')[2].innerHTML="Fahm adnan";
+  document.getElementsByClassName('h2')[2].style.color="green";
+}
+//
+function bullbOff(){
+  document.getElementById('bl').src="images/pic_bulboff.gif";
+}
+
+function bullbOn(){
+  document.getElementById('bl').src="images/pic_bulbon.gif";
+  document.getElementById('bl').style.height="299px";
+
+
+}
